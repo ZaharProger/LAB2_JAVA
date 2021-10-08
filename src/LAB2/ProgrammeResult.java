52edba6abcef5abb4141404245c11c8d465f4c32
Захар Домолего <DomolegoZahar@gmail.com>
2021-10-09 00:37:15 +0800

    Создал класс для хранения результата работы программы/подпрограммы

---
+++ src/LAB2/ProgrammeResult.java
@@ -0,0 +1,32 @@
+package LAB2;
+
+public class ProgrammeResult{
+    private String message;
+    private boolean isSuccessful;
+
+    public ProgrammeResult(){
+        message = "Не были получены входные данные!";
+        isSuccessful = false;
+    }
+
+    public ProgrammeResult(String message, boolean isSuccessful){
+        this.message = message;
+        this.isSuccessful = isSuccessful;
+    }
+
+    public void setMessage(String message){
+        this.message = message;
+    }
+
+    public void setSuccessStatus(boolean status){
+        isSuccessful = status;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+
+    public boolean getSuccessStatus(){
+        return isSuccessful;
+    }
+}
