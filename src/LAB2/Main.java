272745b8e94a778141cbac7c655b0465412e9d1b
Захар Домолего <DomolegoZahar@gmail.com>
2021-10-10 22:50:40 +0800

    Revert "Добавил пояснения и небольшие правки"
    This reverts commit e8b3ccb56c2944fc840c2d1c0aeab712151e3df8.

--- src/LAB2/Main.java
+++ src/LAB2/Main.java
@@ -3,22 +3,20 @@ package LAB2;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Files;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        //Реализованный интерфейс выборки меню, для выбора меню используется уникальный код.
-        // По коду текст меню подгружается с файла
         Launchable menuManager = (short code)->{
             String data;
             try{
                 data = switch (code) {
-                    case -1 -> Files.readString(Path.of("src//LAB2//mainMenu.txt"));
-                    case 1 -> Files.readString(Path.of("src//LAB2//task1.txt"));
-                    case 2 -> Files.readString(Path.of("src//LAB2//task2.txt"));
-                    case 0 -> "Завершение работы...";
+                    case 1 -> Files.readString(Path.of("src//LAB2//mainMenu.txt"));
+                    case 2 -> Files.readString(Path.of("src//LAB2//task1.txt"));
+                    case 3 -> Files.readString(Path.of("src//LAB2//task2.txt"));
                     default -> "Такой команды не существует!";
                 };
             }
@@ -27,6 +25,7 @@ public class Main {
             }
             return data;
         };
+
 
     }
 }
