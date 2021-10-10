e94b5bdcb78956f9ecd290ee67661553fedba188
Захар Домолего <DomolegoZahar@gmail.com>
2021-10-10 11:08:38 +0800

    Revert "Создал интерфейс для реализации проверки ввода"
    This reverts commit 6201c096e1e0ca3adc72b6cb4cbbe3ab6756a780.

--- src/LAB2/Analyzable.java
+++
@@ -1,5 +0,0 @@
-package LAB2;
-//Интерфейс анализируемых объектов (используется для реализации проверки ввода в виде лямбда-выражений)
-public interface Analyzable<T> {
-    ProgrammeResult analyze(T inputData);
-}
